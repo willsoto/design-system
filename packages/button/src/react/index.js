9c3ef36f886f3cad578b93b2cf65405875b3ce24
Dane Thurber <dane.thurber@gmail.com>
2019-12-06 10:23:43 -0700

    refactor(button): use jsx; compose styles; general cleanup

--- packages/button/src/react/index.js
+++ packages/button/src/react/index.js
@@ -1,8 +1,9 @@
-import filterReactProps from '@pluralsight/ps-design-system-filter-react-props'
-import { css } from 'glamor'
-import Icon, { sizes as iconSizes } from '@pluralsight/ps-design-system-icon'
+import { compose, css } from 'glamor'
 import PropTypes from 'prop-types'
 import React from 'react'
+
+import filterReactProps from '@pluralsight/ps-design-system-filter-react-props'
+import Icon, { sizes as iconSizes } from '@pluralsight/ps-design-system-icon'
 import { useTheme } from '@pluralsight/ps-design-system-theme'
 
 import stylesheet from '../css/index.js'
@@ -13,49 +14,54 @@ const spin = css.keyframes(
 )
 
 const styles = {
-  button: ({
-    appearance,
-    css: cssProp,
-    disabled,
-    icon,
-    iconAlign,
-    iconOnly,
-    loading,
-    size,
-    themeName
-  }) =>
-    css(
-      stylesheet['.psds-button'],
-      stylesheet[`.psds-button--size-${size}`],
-      stylesheet[`.psds-button--appearance-${appearance}`],
-      stylesheet[
-        `.psds-button--appearance-${appearance}.psds-theme--${themeName}`
-      ],
-      disabled && {
-        ...stylesheet[`.psds-button--disabled`],
-        ...stylesheet[`.psds-button--disabled.psds-theme--${themeName}`],
-        ...stylesheet[
-          `.psds-button--disabled.psds-button--appearance-${appearance}`
+  button: (themeName, props) => {
+    const { appearance, disabled, icon, iconAlign, iconOnly, size } = props
+
+    return compose(
+      css(stylesheet['.psds-button']),
+      css(stylesheet[`.psds-button--size-${size}`]),
+      css(stylesheet[`.psds-button--appearance-${appearance}`]),
+      css(
+        stylesheet[
+          `.psds-button--appearance-${appearance}.psds-theme--${themeName}`
         ]
-      },
+      ),
+      disabled &&
+        compose(
+          css(stylesheet[`.psds-button--disabled`]),
+          css(stylesheet[`.psds-button--disabled.psds-theme--${themeName}`]),
+          css(
+            stylesheet[
+              `.psds-button--disabled.psds-button--appearance-${appearance}`
+            ]
+          )
+        ),
       icon &&
-        !iconOnly && {
-          ...stylesheet[
-            `.psds-button--iconAlign-${iconAlign}.psds-button--not-iconOnly`
-          ],
-          ...stylesheet[
-            `.psds-button--iconAlign-${iconAlign}.psds-button--not-iconOnly.psds-button--size-${size}`
-          ]
-        },
+        !iconOnly &&
+        compose(
+          css(
+            stylesheet[
+              `.psds-button--iconAlign-${iconAlign}.psds-button--not-iconOnly`
+            ]
+          ),
+          css(
+            stylesheet[
+              `.psds-button--iconAlign-${iconAlign}.psds-button--not-iconOnly.psds-button--size-${size}`
+            ]
+          )
+        ),
+
+      iconOnly &&
+        compose(
+          css(stylesheet[`.psds-button--iconOnly`]),
+          css(stylesheet[`.psds-button--iconOnly.psds-button--size-${size}`])
+        ),
+
       iconAlign === vars.iconAligns.right &&
-        stylesheet[`.psds-button--iconAlign-${iconAlign}`],
-      iconOnly && {
-        ...stylesheet[`.psds-button--iconOnly`],
-        ...stylesheet[`.psds-button--iconOnly.psds-button--size-${size}`]
-      },
-      cssProp
-    ),
-  loading: ({ appearance, themeName }) =>
+        css(stylesheet[`.psds-button--iconAlign-${iconAlign}`])
+    )
+  },
+  loading: (themeName, { appearance }) =>
     css(
       stylesheet[`.psds-button__loading`]({ spin }),
       stylesheet[`.psds-button__loading--appearance-${appearance}`],
@@ -80,71 +86,79 @@ const mapIconSize = props => {
     [vars.sizes.medium]: iconSizes.medium,
     [vars.sizes.large]: iconSizes.medium
   }
+
   return btnToIconSizes[props.size]
     ? btnToIconSizes[props.size]
     : iconSizes.medium
 }
 
-const renderIcon = props =>
-  props.loading ? (
-    <div {...styles.icon(props)}>
-      <Icon size={mapIconSize(props)}>
-        <span {...styles.loading(props)} />
-      </Icon>
-    </div>
-  ) : props.icon ? (
-    <div {...styles.icon(props)}>
-      {React.cloneElement(props.icon, {
-        size: mapIconSize(props)
-      })}
-    </div>
-  ) : null
+/* eslint-disable react/prop-types */
+const renderIcon = (themeName, props) => {
+  const { loading, icon } = props
 
-renderIcon.propTypes = {
-  loading: PropTypes.bool,
-  icon: PropTypes.element
+  if (loading)
+    return (
+      <div {...styles.icon(props)}>
+        <Icon size={mapIconSize(props)}>
+          <span {...styles.loading(themeName, props)} />
+        </Icon>
+      </div>
+    )
+
+  if (icon)
+    return (
+      <div {...styles.icon(props)}>
+        {React.cloneElement(props.icon, { size: mapIconSize(props) })}
+      </div>
+    )
+
+  return null
 }
+/* eslint-enable react/prop-types */
 
-const Button = React.forwardRef((props, ref) => {
+const Button = React.forwardRef((props, forwardedRef) => {
   const themeName = useTheme()
-  if (!ref) ref = React.useRef()
+
+  const ref = React.useRef()
+  React.useImperativeHandle(forwardedRef, () => ref.current)
+
   const nonLoadingWidth = React.useMemo(() => {
     if (props.loading && ref && ref.current) {
       return ref.current.offsetWidth
     }
   }, [props.loading, ref])
 
-  const tagName = props.href ? 'a' : 'button'
+  const TagName = props.href ? 'a' : 'button'
   const isLoadingWithNoText = !!nonLoadingWidth
+
   const allProps = {
     ...props,
     isLoadingWithNoText,
-    iconOnly: React.Children.count(props.children) <= 0,
-    themeName
+    iconOnly: React.Children.count(props.children) <= 0
   }
 
   const isDisabledLink = allProps.disabled && allProps.href
-  let filteredProps = filterReactProps(props, { tagName })
+  const filteredProps = filterReactProps(props, { tagName: TagName })
   delete filteredProps.icon
-  if (isDisabledLink) {
-    delete filteredProps.onClick
-  }
+  if (isDisabledLink) delete filteredProps.onClick
 
-  return React.createElement(
-    tagName,
-    {
-      ...styles.button(allProps),
-      ...filteredProps,
-      disabled: props.disabled || props.loading,
-      ref,
-      style: isLoadingWithNoText
-        ? { ...props.style, width: nonLoadingWidth }
-        : props.style || {}
-    },
-    renderIcon(allProps),
-    !isLoadingWithNoText && (
-      <span {...styles.text(allProps)}>{allProps.children}</span>
-    )
+  const style = props.style || {}
+  if (isLoadingWithNoText) style.width = nonLoadingWidth
+
+  return (
+    <TagName
+      {...styles.button(themeName, allProps)}
+      {...filteredProps}
+      disabled={props.disabled || props.loading}
+      ref={ref}
+      style={style}
+    >
+      {renderIcon(themeName, allProps)}
+
+      {!isLoadingWithNoText && (
+        <span {...styles.text(themeName, allProps)}>{allProps.children}</span>
+      )}
+    </TagName>
   )
 })
 
@@ -155,11 +169,11 @@ Button.propTypes = {
   href: PropTypes.string,
   icon: PropTypes.element,
   iconAlign: PropTypes.oneOf(Object.keys(vars.iconAligns)),
-  innerRef: PropTypes.func,
   loading: PropTypes.bool,
   size: PropTypes.oneOf(Object.keys(vars.sizes)),
   style: PropTypes.object
 }
+
 Button.defaultProps = {
   appearance: vars.appearances.primary,
   disabled: false,
@@ -167,6 +181,7 @@ Button.defaultProps = {
   loading: false,
   size: vars.sizes.medium
 }
+
 Button.appearances = vars.appearances
 Button.iconAligns = vars.iconAligns
 Button.sizes = vars.sizes
